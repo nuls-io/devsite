ccfb866af608bb3da5fbe275914071c904a5946a
wql77 <2837280443@qq.com>
2018-07-30 18:17:58 +0800

    添加title

--- themes/nuls/source/js/common.js
+++ themes/nuls/source/js/common.js
@@ -5,7 +5,7 @@
     var headerHeight=document.getElementById('header').offsetHeight;
     var footerHeight=document.getElementById('footer').offsetHeight;
     var contentWrapHeight=document.getElementById('content-wrap').style.minHeight;
-        contentWrapHeight=clientHeight-headerHeight-footerHeight+'px';
+    contentWrapHeight=clientHeight-headerHeight-footerHeight+'px';
     //右侧边栏动态添加title
     var tocText=document.getElementsByClassName("toc-text");
     for(var i=0;i<tocText.length;i++){
